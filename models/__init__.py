35863d39a2301b3d348d97c5ec5fc4dbf8f07b2f
Mingdezzi <107346824+Mingdezzi@users.noreply.github.com>
2025-11-15 01:17:19 +0900

    Delete models directory

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from ..extensions import db
-from .auth import Brand, User
-from .store import Store, Staff, ScheduleEvent, Setting, Announcement
-from .product import Product, Variant, StoreStock, StockHistory
-from .sales import Order, OrderProcessing, Sale, SaleItem
-from .store import Comment
